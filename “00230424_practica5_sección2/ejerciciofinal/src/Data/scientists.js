50c7df88415cb5bd1724144c04b944208a9c24a5
dsamuel41 <00230424@uca.edu.sv>
2025-09-30 21:41:54 -0600

    Feat: Subiendo ejercicio final

---
+++ “00230424_practica5_sección2/ejerciciofinal/src/Data/scientists.js
@@ -0,0 +1,19 @@
+import maria from '../assets/maria.jpg';
+import katsuko from '../assets/katsuko.jpg';
+
+export const scientists = [
+  {
+    name: "Maria Slodowska Curie",
+    image: maria,
+    profession: "física y química",
+    awards: "4 (Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci)",
+    discoveries: "polonio(elemento quimico)"
+  },
+  {
+    name: "Katsuko Saruhashi",
+    image: katsuko,
+    profession: "geoquímica",
+    awards: "2 (Premio Miyake de geoquímica, Premio Tanaka)",
+    discoveries: "desarrollo de métodos para medir el dióxido de carbono en el agua"
+  }
+];
